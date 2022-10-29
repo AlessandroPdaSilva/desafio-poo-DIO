bf769a35ab4debbc030bd9baddbe8d9e7247f7ca
Alessandro <alessandropds55@gmail.com>
2022-10-29 15:37:55 -0300

    final

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,9 @@ package main;
 
 import java.time.LocalDate;
 
+import desafio.dominio.Bootcamp;
 import desafio.dominio.Curso;
+import desafio.dominio.Dev;
 import desafio.dominio.Mentoria;
 
 public class Main {
@@ -22,9 +24,44 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        //System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        //System.out.println(mentoria);
+        
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescricao("Descrição do bootcamp Java Developer");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Camila");
+        dev1.inscreverBootcamp(bootcamp1);
+
+        Dev dev2 = new Dev();
+        dev2.setNome("João");
+        dev2.inscreverBootcamp(bootcamp1);
+        
+        System.out.println("Inscritos:" + dev1.getNome() + ":" + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Concluidos" + dev1.getNome() + ":" + dev1.getConteudosConcluidos());
+        System.out.println("Inscritos:" + dev1.getNome() + ":" + dev1.getConteudosInscritos());
+
+        System.out.println("----");
+
+        System.out.println("Inscritos:" + dev2.getNome() + ":" + dev2.getConteudosInscritos());
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println("Concluidos" + dev2.getNome() + ":" + dev1.getConteudosConcluidos());
+        System.out.println("Inscritos:" + dev2.getNome() + ":" + dev2.getConteudosInscritos());
+
+
+        System.out.println("XP Camila:" + dev1.calcularTotalXp());
+        System.out.println("XP Joao:" + dev2.calcularTotalXp());
 
     }
 }
