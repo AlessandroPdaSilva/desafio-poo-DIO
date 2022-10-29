4e1c63506a8022f8480efc22779da3634b3de682
Alessandro <alessandropds55@gmail.com>
2022-10-29 15:25:11 -0300

    Bootcamp, Conteudo, Dev classes criadas

--- src/desafio/dominio/Curso.java
+++ src/desafio/dominio/Curso.java
@@ -1,29 +1,13 @@
 package desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
-    private int cargaHoraria;
+public class Curso extends Conteudo{
 
-    public Curso() {
+	private int cargaHoraria;
+	
+	public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
+	
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -32,12 +16,17 @@ public class Curso {
         this.cargaHoraria = cargaHoraria;
     }
 
-    @Override
-    public String toString() {
-        return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cargaHoraria=" + cargaHoraria +
-                '}';
-    }
+    
+	@Override
+	public String toString() {
+		return "Curso [cargaHoraria=" + cargaHoraria + "]";
+	}
+
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
+	}
+
+	
+	
 }
